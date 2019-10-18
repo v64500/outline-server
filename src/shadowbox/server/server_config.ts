2ae5752e210e21fd065be47a065f24b368866d7a
Jon Cohen <cohenjon@google.com>
2019-10-18 16:49:06 -0400

    Respond to Review Comments
    At some point down the line I royally screwed up my git history and
    reverted a bunch of changes.  I'm not sure what or when, so I `git reset --hard 1cd28e4` and tried to redo my
    past responses to comments from there.

--- src/shadowbox/server/server_config.ts
+++ src/shadowbox/server/server_config.ts
@@ -51,6 +51,7 @@ export function readServerConfig(filename: string): json_config.JsonConfig<Serve
     config.data().serverId = config.data().serverId || uuidv4();
     config.data().metricsEnabled = config.data().metricsEnabled || false;
     config.data().createdTimestampMs = config.data().createdTimestampMs || Date.now();
+    config.data().hostname = config.data().hostname || process.env.SB_PUBLIC_IP;
     config.write();
     return config;
   } catch (error) {
